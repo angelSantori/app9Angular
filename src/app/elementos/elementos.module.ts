012560dd3585181a6a251c54ddca6bb51b49eb57
angelSantori <antonyo.da.mm@gmail.com>
2023-11-02 12:59:04 -0700

    Export DadoComponent

--- src/app/elementos/elementos.module.ts
+++ src/app/elementos/elementos.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DadoComponent } from './dado/dado.component';
 
-
-
 @NgModule({
   declarations: [
     DadoComponent
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [DadoComponent] //<---Agregado
 })
 export class ElementosModule { }
