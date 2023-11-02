a2d2303c3914864115d1fbaae2f20312b4c8a2ce
angelSantori <antonyo.da.mm@gmail.com>
2023-11-02 12:53:52 -0700

    TS dado

--- src/app/elementos/dado/dado.component.ts
+++ src/app/elementos/dado/dado.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dado',
   templateUrl: './dado.component.html',
   styleUrls: ['./dado.component.css']
 })
-export class DadoComponent {
+export class DadoComponent implements OnInit{
+  @Input() valor: number=1;
 
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
